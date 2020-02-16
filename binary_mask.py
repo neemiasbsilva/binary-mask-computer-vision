12321cecb9aa576bb88f01dac0e288e493cc6ca2
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-15 21:52:14 -0400

    Implementation of Binary mask by hand

--- binary_mask.py
+++ binary_mask.py
@@ -4,6 +4,7 @@ import cv2
 def binary_mask(path, r, g, b, k):
     # read the image
     img = cv2.imread(path)
+    print(img.shape)
     # convert the channels of image
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
