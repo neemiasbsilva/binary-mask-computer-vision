1575fd3c972fcdbddb6773775b581788b2b25850
neemiasbsilva <neemiasbsilva@gmail.com>
2020-02-15 21:28:25 -0400

    Get the 4 value of parameter & path of image

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 from binary_mask import binary_mask
 
+def main():
+
+    print("Choose the path of image for binarize:", end=' ')
+    path = str(input())
+    print("Choose the value of parameter r:", end=' ')
+    r = float(input())
+    print("Choose the value of parameter g:", end=' ')
+    g = float(input())
+    print("Choose the value of parameter b:", end=' ')
+    b = float(input())
+    print("Choose the value of parameter k:", end=' ')
+    k = float(input())
+
+    img_binary = binary_mask(path, r, g, b, k)    
+
+if __name__ == "__main__":
+    main()
